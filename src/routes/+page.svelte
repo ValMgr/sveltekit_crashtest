bb13cb3b72023dd1295ec0962db709f8cee81d5c
Valentin Magry <valou32720@gmail.com>
2022-11-09 16:44:33 +0100

    [FEAT] Clean and add some content to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,15 +4,50 @@
 	const isLogged: boolean = !!$session;
 </script>
 
-<h1>Welcome to LVA</h1>
+<h2>Welcome to LVA</h2>
+<p>
+	This is a crashtest app to experiment SvelteKit. <br />
+	Goal is to create a simple app to manage student lists.
+</p>
 <p>Powered with SvelteKit</p>
+
+<hr />
+
+<h5>Basic features:</h5>
+<ul>
+	<li>- Basic auth system</li>
+	<li>- List student from JSON file</li>
+	<li>- Searchbar to find student</li>
+	<li>- Sorts student by several criteria</li>
+</ul>
+
+<p></p>
 
 <hr />
 <p>
 	Auth status:
 	{#if isLogged}
-		You are logged in
+		<span style:color="{'green'}"> You are logged in</span>
 	{:else}
-		You are not logged in
+		<span style:color="{'red'}"> You are not logged in</span>
 	{/if}
 </p>
+
+<style>
+	h2 {
+		font-size: 1.8rem;
+		font-weight: 600;
+		margin-bottom: 2rem;
+	}
+
+	h5 {
+		font-size: 1.1rem;
+		margin-bottom: 1rem;
+		text-decoration: underline;
+	}
+
+	hr {
+		margin: 2rem 0;
+	}
+
+</style>
